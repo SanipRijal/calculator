edfc17ce6366754f14ed729f28161a9c818ab541
SanipRijal <saneprijal@gmail.com>
2018-01-06 08:12:58 +0545

    This is a first commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+#returns sum of two numbers
+def sum(a, b):
+	return (a + b)
+
+#returns difference of two numbers
+def sub(a, b):
+	return (a - b)
+
+#returns multiplication of two numbers
+def mul(a, b):
+	return (a * b)
+
+#returns division of two numbers
+def div(a, b):
+	return (a / b) 
