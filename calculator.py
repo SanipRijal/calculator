58f51c07afa298cb5f1760cf0e8601bbddf5e81b
SanipRijal <saneprijal@gmail.com>
2018-01-10 11:01:38 +0545

    Refined commit

--- calculator.py
+++ calculator.py
@@ -8,9 +8,31 @@ repeat = 'y'
 #loop while the user doesnot has to do more calculations
 while(repeat != 'n'):
 	#ask the numbers from the user
-	num1 = int(input("Enter a number: "))
-	num2 = int(input("Enter another number: "))
 
+	
+	num1Err = num2Err = 1	#declare two variables to keep track if error occurs. 1 for error occured, 0 for no error
+
+
+	while(num1Err != 0):	#repeat until the user inputs the correct type of input for first number
+
+		#check if the input number is not a valid real number
+		try:
+			num1 = float(input("1st Number: "))
+			num1Err = 0	#change the error variable to indicate no error occured
+
+		#handle the exception if the user enters a value other than a valid real number
+		except ValueError:
+			print("Enter a valid number.")
+
+	while(num2Err != 0):	#repeat until the user inputs the correct type of input for the second number
+
+		try:
+			num2 = float(input("2nd Number: "))
+			num2Err = 0	#change the error variable to indicate no error occured
+
+		except ValueError:
+			print("Enter a valid number.")
+		
 	#print the menu to the user
 	print("Operations: \n>+\n>-\n>*\n>/")
 	
@@ -36,8 +58,11 @@ while(repeat != 'n'):
 	elif(operator == '/'):
 		result = div(num1, num2)
 		print("The division of {} and {} is {}".format(num1, num2, result))
+	
+	#if invalid choice of operation
 	else:
 		print("Enter a valid character(+, -, *, /)")
 	
+	#prompt the user if he/she wants to continue or not
 	repeat = input("Do you want to continue(y/n)?")
 
